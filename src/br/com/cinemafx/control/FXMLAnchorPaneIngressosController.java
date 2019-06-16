9f375966f9b1ecfd87013ced81736f8c3d05d417
Felipe Monay <felipemonay@gmail.com>
2019-06-16 15:53:19 -0300

    Ingressos

--- src/br/com/cinemafx/control/FXMLAnchorPaneIngressosController.java
+++ src/br/com/cinemafx/control/FXMLAnchorPaneIngressosController.java
@@ -1,17 +1,27 @@
 
 package br.com.cinemafx.control;
 
+import br.com.cinemafx.bean.Filme;
 import br.com.cinemafx.bean.Ingresso;
+import br.com.cinemafx.dao.IngressoDAO;
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
 public class FXMLAnchorPaneIngressosController implements Initializable {
@@ -43,10 +53,107 @@ public class FXMLAnchorPaneIngressosController implements Initializable {
     
     private List<Ingresso> listIngresso;
     private ObservableList<Ingresso> observableListIngresso;
+    
+    private final IngressoDAO ingressoDAO = new IngressoDAO();
+    
      
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        carregarTableViewIngresso();
+        
+        //Listen acionado diante de quaisquer alteracoes na selecao de itens do TableView
+        tableViewIngresso.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> selecionarItemTableViewIngresso(newValue));
+    }
     
+    public void carregarTableViewIngresso(){
+        tableColumnIngressoCodigo.setCellValueFactory(new PropertyValueFactory<>("codigo"));
+        tableColumnIngressoSessao.setCellValueFactory(new PropertyValueFactory<>("sessao"));
+        tableColumnIngressoTotal.setCellValueFactory(new PropertyValueFactory<>("total"));
+        
+        listIngresso = IngressoDAO.select();
+        
+        observableListIngresso = FXCollections.observableArrayList(listIngresso);
+        tableViewIngresso.setItems(observableListIngresso);
+    }
+    
+     public void selecionarItemTableViewIngresso(Ingresso i){
+        if(i != null){
+            labelIngressoCodigo.setText(String.valueOf(i.getIdIngresso()));
+            labelIngressoSessao.setText(String.valueOf(i.getIdSessao()));
+            labelIngressoQuantidade.setText(String.valueOf(i.getQuantidade()));
+            labelIngressoTotal.setText(String.valueOf(i.getTotal()));
+            labelIngressoFormaPgmt.setText(String.valueOf(i.getFormaPgmt()));
+        }else{
+            labelIngressoCodigo.setText("");
+            labelIngressoSessao.setText("");
+            labelIngressoQuantidade.setText("");
+            labelIngressoTotal.setText("");
+            labelIngressoFormaPgmt.setText("");
+        }
+    }
+    
+    @FXML
+    public void handleButtonInserir() throws IOException{
+        Ingresso i = new Ingresso();
+        boolean buttonConfirmarClicked = showFXMLAnchorPaneCadastrosIngressoDialog(i);
+        if(buttonConfirmarClicked){
+            ingressoDAO.insert(i);
+            carregarTableViewIngresso();
+        }
+    }
+    
+    @FXML
+    public void handleButtonAlterar() throws IOException {
+        Ingresso i = tableViewIngresso.getSelectionModel().getSelectedItem();
+        if(i != null){
+            boolean buttonConfirmarClicked = showFXMLAnchorPaneCadastrosIngressoDialog(i);
+            if(buttonConfirmarClicked){
+                ingressoDAO.update(i);
+                carregarTableViewIngresso();
+            }
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Por favor, escolha uma venda na Tabela");
+            alert.show();
+        }
+    }
+    
+    @FXML
+    public void handleButtonRemover() throws IOException {
+        Ingresso i = tableViewIngresso.getSelectionModel().getSelectedItem();
+        if(i != null){
+            ingressoDAO.delete(i);
+            carregarTableViewIngresso();
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Por favor, escolha uma venda na Tabela");
+            alert.show();
+        }
+    }
+    
+    public boolean showFXMLAnchorPaneCadastrosIngressoDialog(Ingresso i) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(FXMLAnchorPaneIngressosDialogController.class.getResource("/br/com/cinemafx/view/FXMLAnchorPaneIngressosDialog.fxml"));
+        AnchorPane page = (AnchorPane) loader.load();
+        
+        //Criando o Stage de Dialog
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("Cadastro de Ingressos");
+        Scene scene = new Scene(page);
+        dialogStage.setScene(scene);
+        
+        //Setando o cliente no Control
+        FXMLAnchorPaneIngressosDialogController controller = loader.getController();
+        controller.setDialogStage(dialogStage);
+        controller.setIngresso(i);
+        
+        //Mostra o Dialog e espera ate que o user feche
+        dialogStage.showAndWait();
+        
+        return controller.isButtonConfirmarClicked();
+    }
+    
+}
+  
 }
